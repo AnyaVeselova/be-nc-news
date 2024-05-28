44063c6a62779f56718e51b46129f3b06848051f
AnyaVeselova <anna.correspondence@outlook.com>
2024-05-28 20:26:55 +0100

    creates a controller and a model for /api/articles/:article_id endpoint; creates test suit for a happy path

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -44,6 +44,26 @@ describe("GET /api", () => {
   });
 });
 
+describe.only("GET /api/articles/:article_id", () => {
+  test("GET: 200 sends a single article to the client ", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.article.article_id).toBe(1);
+        expect(body.article.title).toBe("Living in the shadow of a great man");
+        expect(body.article.topic).toBe("mitch");
+        expect(body.article.author).toBe("butter_bridge");
+        expect(body.article.body).toBe("I find this existence challenging");
+        expect(body.article.created_at).toBe("2020-07-09T20:11:00.000Z");
+        expect(body.article.votes).toBe(100);
+        expect(body.article.article_img_url).toBe(
+          "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
+        );
+      });
+  });
+});
+
 describe("Generic errors", () => {
   test("404: responds with not found error when endpoint does not exist  ", () => {
     return request(app)
--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { allTopics } = require("./controllers/topics.controllers");
 const { allEndpoints } = require("./controllers/endpoints.controllers");
+const { getArticleById } = require("./controllers/getArticle.controllers");
 const express = require("express");
 
 const app = express();
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.get("/api/topics", allTopics);
 app.get("/api", allEndpoints);
+app.get("/api/articles/:article_id", getArticleById);
 
 app.all("*", (req, res) => {
   res.status(404).send({ msg: "Route not found" });
