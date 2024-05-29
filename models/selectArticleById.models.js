024269e9b52c17d43c6ac2d3442fd103c1838b15
AnyaVeselova <anna.correspondence@outlook.com>
2024-05-29 10:35:08 +0100

    destructures rows in a parameter - code refactoring

--- models/selectArticleById.models.js
+++ models/selectArticleById.models.js
@@ -3,7 +3,7 @@ const db = require("../db/connection");
 exports.selectArticleById = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
-    .then((result) => {
-      return result.rows[0];
+    .then(({ rows }) => {
+      return rows[0];
     });
 };
