e6ce50045d05807fd2510246a88b2f81530e8762
AnyaVeselova <anna.correspondence@outlook.com>
2024-06-02 15:15:26 +0100

    deletes unnesessary line of code

--- routes/endpoints-router.js
+++ routes/endpoints-router.js
@@ -1,6 +1,5 @@
 const endpointsRouter = require("express").Router();
 
-const e = require("express");
 const { allEndpoints } = require("../controllers/endpoints.controllers");
 
 endpointsRouter.get("/", allEndpoints);
