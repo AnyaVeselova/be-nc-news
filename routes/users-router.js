97ed17c56d6ae22336cde086ca63b21231de551c
AnyaVeselova <anna.correspondence@outlook.com>
2024-10-03 17:25:01 +0100

    chore: updates login route

--- routes/users-router.js
+++ routes/users-router.js
@@ -3,11 +3,10 @@ const usersRouter = require("express").Router();
 const {
   getAllUsers,
   getUserByUsername,
-
   loginUser,
 } = require("../controllers/users.controllers");
 
 usersRouter.get("/", getAllUsers);
 usersRouter.get("/:username", getUserByUsername);
-usersRouter.post("/login", loginUser);
+usersRouter.post("/:login", loginUser);
 module.exports = usersRouter;
